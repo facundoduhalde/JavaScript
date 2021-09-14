b3eb57929a41001845a613a33081b66fb58c78fe
facundoduhalde <facundo.duhalde@gmail.com>
2021-09-14 07:42:14 -0300

    Primer commit Curso JavaScript

---
+++ CoderHouse/Clase 1/app.js
@@ -0,0 +1,15 @@
+//Desafio Clase 1 
+
+let nombre = prompt('Ingrese su nombre');
+let apellido = prompt('Ingrese su apellido');
+
+console.log(`${nombre} ${apellido}`);
+
+let numero1 = Number(prompt("Ingrese el primer número"));
+let numero2 = Number(prompt("Ingrese el segundo número"));
+
+console.log(numero1 + numero2);
+console.log(numero1 - numero2);
+console.log(numero1 / numero2);
+console.log(numero1 * numero2);
+
