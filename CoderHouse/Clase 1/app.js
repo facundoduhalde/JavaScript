a61a6fb671650158be5d225c5a853161d1aca8fb
facundoduhalde <facundo.duhalde@gmail.com>
2021-09-14 07:48:51 -0300

    Desafio Clase 1

--- CoderHouse/Clase 1/app.js
+++ CoderHouse/Clase 1/app.js
@@ -1,13 +1,17 @@
 //Desafio Clase 1 
 
+// Variables mediante Prompt para Nombre y Apellido
 let nombre = prompt('Ingrese su nombre');
 let apellido = prompt('Ingrese su apellido');
 
+//Muestro en consola la concatenacion del Nombre y Apellido
 console.log(`${nombre} ${apellido}`);
 
+// Variables mediante Prompt de 2 números, parseados a tipo de dato numerico 
 let numero1 = Number(prompt("Ingrese el primer número"));
 let numero2 = Number(prompt("Ingrese el segundo número"));
 
+//Muestro en consola el resultado de la suma, resta, division y multiplicacion de ambas variables
 console.log(numero1 + numero2);
 console.log(numero1 - numero2);
 console.log(numero1 / numero2);
